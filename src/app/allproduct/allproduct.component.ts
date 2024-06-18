333d50cca0fc5fc8ee45b81a1d61dd87ec016bb8
omarwagdy10 <omarwagdy122@gmail.com>
2024-06-18 18:26:04 +0300

    fix res

--- src/app/allproduct/allproduct.component.ts
+++ src/app/allproduct/allproduct.component.ts
@@ -3,40 +3,28 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbRatingConfig, NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
-import {MatSliderModule} from '@angular/material/slider';
+import { MatSliderModule } from '@angular/material/slider';
 
 @Component({
   selector: 'app-allproduct',
   standalone: true,
-  imports: [NgbRatingModule,MatSliderModule],
+  imports: [NgbRatingModule, MatSliderModule],
   templateUrl: './allproduct.component.html',
-  styleUrl: './allproduct.component.css'
+  styleUrl: './allproduct.component.css',
 })
 export class AllproductComponent implements OnInit {
+  constructor(private http: HttpClient, private router: Router) {}
 
-  constructor (private http : HttpClient, private router : Router  ){}
-
-  allproducts!:any
+  allproducts!: any;
   filteredProducts: any[] = [];
   selectedCategory: string = '';
-
-
-
   ngOnInit(): void {
-
-    this.http.get('https://fakestoreapi.com/products').subscribe((res)=>{
-      this.allproducts = res
-    })
-    
-    
+    this.http.get('https://fakestoreapi.com/products').subscribe((res) => {
+      this.allproducts = res;
+    });
   }
 
-
-  redirect(product_id : any){
-    this.router.navigate(['product_details',product_id])
+  redirect(product_id: any) {
+    this.router.navigate(['product_details', product_id]);
   }
-
-
 }
-
-
